4b4662f31fe6e4455bf85de9954b4aebbf46e5b6
Tanvirnawab <tantanvirshaikh@gmail.com>
2026-02-18 10:05:07 +0530

    Fix production database config using DATABASE_URL

--- clinical_registry/settings.py
+++ clinical_registry/settings.py
@@ -84,14 +84,9 @@ WSGI_APPLICATION = "clinical_registry.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'clinical_db'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
+     'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
--- trials/models.py
+++ trials/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-
 from django.contrib.auth.models import User
+
 
 class ClinicalTrial(models.Model):
 
@@ -21,10 +19,24 @@ class ClinicalTrial(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     phase = models.CharField(max_length=20, choices=PHASE_CHOICES)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='Pending'
+    )
     start_date = models.DateField()
     end_date = models.DateField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="clinical_trials"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']  # newest first automatically
 
     def __str__(self):
         return self.title
